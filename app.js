7c8fec5a0222979073b7dd8a7acd94d6059f3487
Aadarsh Kushwaha <aadarshkus555@gmail.com>
2025-07-31 02:04:29 +0530

    Deletion of reviews

--- app.js
+++ app.js
@@ -121,11 +121,21 @@ await newReview.save();
 await listing.save();
 
 res.redirect(`/listings/${listing._id}`);
-
-
 }
 )
 );
+
+// Delete the review one by one from the listing
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res)=>{
+  let {id,reviewId} = req.params;
+
+  await Listing.findByIdAndUpdate(id,{$pull: {reviews : reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+res.redirect(`/listings/${id}`);
+
+}));
+
 
 
 
